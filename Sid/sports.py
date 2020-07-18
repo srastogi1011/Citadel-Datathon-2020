bb6334c55879f43a29c67e26a7c4f96ac68baab6
Sidhant Rastogi <srastogi1011@gmail.com>
2020-07-18 19:07:39 -0400

    Updated graphs

--- Sid/sports.py
+++ Sid/sports.py
@@ -7,23 +7,40 @@ import os
 
 plt.style.use('ggplot')
 
-filepath = os.path.abspath('datasets_full/London/london_sports_participation.csv')
+filepath = os.path.abspath('london_sports_participation_cleaned.csv')
 sports_df = pd.read_csv(filepath)
 print(sports_df)
 
+years = sports_df['year'].unique().tolist()
+ind = np.arange(len(years))
+
 regions = sports_df['area'].unique().tolist()
-non_regions = ['East Midlands', 'East', 'England', 'London', 'North East', 'North West', 'South East', 'South West', 'West Midlands', 'Yorkshire', 'Yorkshire And The Humber']
-[regions.remove(non_region) for non_region in non_regions]
+#non_regions = ['East Midlands', 'East', 'England', 'London', 'North East', 'North West', 'South East', 'South West', 'West Midlands', 'Yorkshire', 'Yorkshire And The Humber']
+#[regions.remove(non_region) for non_region in non_regions]
 
 significant_regions = []
 
 for region in regions:
     region_df = sports_df.loc[sports_df['area'] == region]
-    zeroes = np.array(region_df[region_df['sports_participation'] == 'zero']['percentage'].tolist())
-    ones = np.array(region_df[region_df['sports_participation'] == 'one+']['percentage'].tolist())
-    threes = np.array(region_df[region_df['sports_participation'] == 'three+']['percentage'].tolist())
-    ones = ones - threes
+    zeroes = np.array(region_df[region_df['sports_participation'] == 0]['percentage'].tolist())
+    ones = np.array(region_df[region_df['sports_participation'] == 1]['percentage'].tolist())
+    threes = np.array(region_df[region_df['sports_participation'] == 2]['percentage'].tolist())
+    #ones = ones - threes
+    
+    plt.bar(ind, zeroes, width=0.8, label='0', color='red')
+    plt.bar(ind, ones, width=0.8, label='1+', color='green', bottom=zeroes)
+    plt.bar(ind, threes, width=0.8, label='3+', color='blue', bottom=zeroes+ones)
 
+    plt.xticks(ind, years, rotation=30, fontsize='small')
+    plt.ylabel('Percentages')
+    plt.xlabel('Years')
+    plt.legend(loc='best')
+    plt.title(region + ' Region Sports Participation')
+
+    plt.savefig(region + '.png')
+    #plt.show()
+
+    '''
     zeroes_before = zeroes[0:5]
     zeroes_after = zeroes[5:]
     ones_before = ones[0:5]
@@ -43,3 +60,4 @@ for region in regions:
         significant_regions.append((region, 'threes', ('decrease' if threes_ttest[0] < 0 else 'increase', threes_ttest[1])))
 
 print(significant_regions)
+'''
